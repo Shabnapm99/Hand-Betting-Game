20897f222d58330a7560c778ffebfb6c76fd413d
Shabna P M <pmshabnashareefa@gmail.com>
2026-05-14 21:30:34 +0300

    rectified build issue while deploying

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='py-6 bg-[#132418] italic text-center'>
-      <p className='text-on-surface-variant text-sm'>&copy; 2026 MahjongHandBetting💚Sophisticated</p></footer>
+
+      <p className='text-on-surface-variant text-sm'>&copy; 2026 MahjongHandBetting💚Sophisticated</p>
+
+    </footer>
   )
 }
 
--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 function Root() {
     return (
